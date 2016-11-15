16f11a5b1d56d0b4fb65fe5af8dd8e6b8eef68c6
Andrey Zac <az@stepcart.com>
2016-11-15 16:26:02 +0200

    fix

--- view/adminhtml/web/js/attribute/value/static.js
+++ view/adminhtml/web/js/attribute/value/static.js
@@ -1,5 +1,16 @@
-/* eslint-disable no-undef */
-// jscs:disable jsDoc
+/**
+ * GoMage.com
+ *
+ * GoMage Feed Pro M2
+ *
+ * @category     Extension
+ * @copyright    Copyright (c) 2010-2016 GoMage.com (https://www.gomage.com)
+ * @author       GoMage.com
+ * @license      https://www.gomage.com/licensing  Single domain license
+ * @terms of use https://www.gomage.com/terms-of-use
+ * @version      Release: 1.0.0
+ * @since        Class available since Release 1.0.0
+ */
 
 define([
     'jquery',
