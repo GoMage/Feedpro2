b477caf757cdaaed96c6089071a67f98d4385eaa
RomanKis <roman.kis.y@gmail.com>
2018-06-04 11:49:45 +0300

    FP2-14: Category ID & htmlspecialchars issue

--- Model/Output/HtmlSpecialChars.php
+++ Model/Output/HtmlSpecialChars.php
@@ -16,22 +16,14 @@
 
 namespace GoMage\Feed\Model\Output;
 
-interface OutputInterface
+class HtmlSpecialChars implements OutputInterface
 {
-    const NONE = '';
-    const INTEGER = 'integer';
-    const FLOATS = 'float';
-    const STRIP_TAGS = 'strip_tags';
-    const SPECIAL_ENCODE = 'special_encode';
-    const SPECIAL_DECODE = 'special_decode';
-    const DELETE_SPACE = 'delete_space';
-    const BIG_TO_SMALL = 'big_to_small';
-    const REMOVE_LINE_BREAK = 'remove_lb';
-    const HTML_SPECIAL_CHARS_ENCODE = 'htmlspecialchars';
-
     /**
      * @param  $value
-     * @return mixed
+     * @return string
      */
-    public function format($value);
+    public function format($value)
+    {
+        return htmlspecialchars($value);
+    }
 }
