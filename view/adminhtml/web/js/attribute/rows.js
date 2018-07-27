38de0ab9d785a474591b0de6f451d6dc31708db7
Dmitry Teteruk <dteteruk@gomage.com>
2018-07-27 13:57:49 +0300

    fix js remove

--- view/adminhtml/web/js/attribute/rows.js
+++ view/adminhtml/web/js/attribute/rows.js
@@ -15,22 +15,18 @@
 define([
     'jquery',
     'mage/template',
-    'mage/collapsible',
-    'GoMage_Feed/js/attribute/conditions',
-    'GoMage_Feed/js/attribute/value',
+    'mage/translate',
     'jquery/ui',
     'prototype'
-], function (jQuery, mageTemplate, collapsible, Conditions, Value) {
+], function (jQuery, mageTemplate, translate) {
     'use strict';
 
     return function (config) {
         var Rows = {
             container: $('rows-container'),
             template: mageTemplate('#row-template'),
-            count: 0,
+            titleTemplate: mageTemplate('#title-template'),
             index: 0,
-            conditions: [],
-            values: [],
             init: function () {
                 config.rowsData.forEach(function (data) {
                     this.add(data);
@@ -43,83 +39,178 @@ define([
             add: function (data) {
                 var element;
                 data.row_id = this.index;
+                data.output = (typeof data.output == 'undefined') ? [] : data.output;
                 element = this.template({
                     data: data
                 });
                 Element.insert(this.container, element);
-
-                this.conditions[this.index] = Conditions({
-                    url: config.url,
-                    row_id: this.index,
-                    conditionsData: (typeof data.conditions == 'undefined') ? [] : data.conditions
-                });
-
-                var _data = {
-                    row_id: this.index,
-                    type: (typeof data.type == 'undefined') ? 2 : data.type
-                };
-                if (typeof data.value != 'undefined') {
-                    _data.value = data.value;
-                }
-                this.values[this.index] = Value(_data);
-
-                jQuery(this.container).find('.actions-select').collapsible({ "active": false, "animate": 200, "collapsible": true});
-
-                this.count++;
+                this.setValues();
+                this.setTitle(data.row_id);
                 this.index++;
             },
             remove: function (event) {
-                if (this.count == 1) {
-                    return;
-                }
-                var element = $(Event.findElement(event, 'tr'));
+                var element = $(Event.findElement(event, 'div.fm-block'));
                 if (element) {
                     Element.remove(element);
-                    this.count--;
                 }
+            },
+            toggleEdit: function (event) {
+                var element = $(Event.findElement(event, 'div.fm-block'));
+                if (element) {
+                    var row_id = element.readAttribute('data-row-id');
+                    this.setTitle(row_id);
+                    this.validateElement(element);
+                    element.toggleClassName('__opened');
+                    self = row_id;
+                }
+                this.container.childElements().forEach(function (data) {
+                    if (self !== data.readAttribute('data-row-id')) {
+                        if (data.classList.contains('__opened')) {
+                            this.validateElement(data);
+                            data.classList.remove('__opened');
+                        }
+                    }
+                }, this);
             },
             changeType: function (event) {
                 var element = $(Event.findElement(event, 'select'));
                 if (element) {
-                    var row_id = element.readAttribute('data-row-id');
-                    this.values[row_id] = Value({
-                        row_id: row_id,
-                        type: element.getValue()
-                    });
+                    this.setValue(element);
                 }
             },
-            addCondition: function (event) {
-                var element = $(Event.findElement(event, 'button'));
-                if (element) {
-                    var row_id = element.readAttribute('data-row-id');
-                    this.conditions[row_id].add({
-                            row_id: row_id
-                        }
-                    );
-                }
-            },
-            addValue: function (event) {
-                var element = $(Event.findElement(event, 'button'));
-                if (element) {
-                    var row_id = element.readAttribute('data-row-id');
-                    if ((typeof this.values[row_id] != 'undefined')) {
-                        if (typeof this.values[row_id].object != 'undefined') {
-                            this.values[row_id].object.add({
-                                    row_id: row_id
-                                }
-                            );
-                        }
+            setValues: function () {
+                this.container.select('.type-select').forEach(function (typeField) {
+                    if (!typeField.readAttribute('data-loaded')) {
+                        typeField.writeAttribute('data-loaded', true);
+                        this.setValue(typeField);
                     }
+                }, this);
+            },
+            setValue: function (typeField) {
+                var elementName = typeField.readAttribute('data-value');
+                var input = jQuery("input[name='" + elementName + "']"),
+                    select = jQuery("select[name='" + elementName + "']"),
+                    values = [];
+                switch (parseInt(typeField.getValue())) {
+                    case 2:
+                        values = [];
+                        break;
+                    case 8:
+                    case 9:
+                        values = config.dynamicAttributes;
+                        break;
+                    default:
+                        values = config.attributes;
                 }
+
+                if (values.length) {
+                    input.attr('disabled', 'disabled').hide();
+                    select.removeAttr('disabled').show().find('option').remove();
+                    select.append(jQuery("<option></option>")
+                        .attr("value", "")
+                        .text(translate("Not Set")));
+                    values.forEach(function (data) {
+                        select.append(jQuery("<option></option>")
+                            .attr("value", data.value)
+                            .text(data.label));
+                    });
+                    if (input.val()) {
+                        select.val(input.val());
+                        input.val('');
+                    }
+                } else {
+                    input.removeAttr('disabled').show();
+                    select.attr('disabled', 'disabled').hide();
+                }
+
+            },
+            setTitle: function (row_id) {
+                var data = {},
+                    element,
+                    container = $('title-container-' + row_id);
+
+                data.name = this.getTitleValue(row_id, 'name');
+                if (!data.name) {
+                    data.name = translate('New Row');
+                }
+
+                data.type = this.getTitleValue(row_id, 'type');
+                data.value = this.getTitleValue(row_id, 'value');
+
+                data.prefix_type = this.getTitleValue(row_id, 'prefix_type');
+                data.prefix_value = this.getTitleValue(row_id, 'prefix_value');
+
+                data.suffix_type = this.getTitleValue(row_id, 'suffix_type');
+                data.suffix_value = this.getTitleValue(row_id, 'suffix_value');
+
+                element = this.titleTemplate({
+                    data: data
+                });
+                if(container) {
+                    container.innerHTML = '';
+                    Element.insert(container, element);
+                }
+
+            },
+            getTitleValue: function (row_id, name) {
+                var input = jQuery("input[name='" + config.htmlName + "[" + row_id + "][" + name + "]']"),
+                    select = jQuery("select[name='" + config.htmlName + "[" + row_id + "][" + name + "]']");
+                if (!select.length || select.is(':disabled')) {
+                    return input.val();
+                }
+                if (select.val()) {
+                    return select.find('option:selected').text();
+                }
+                return '';
             },
             bindActions: function () {
                 Event.observe('add_new_row_button', 'click', this.add.bind(Rows, {}));
                 this.container.on('click', '.delete-row', this.remove.bind(this));
+                this.container.on('click', '.fm-block-title', this.toggleEdit.bind(this));
                 this.container.on('change', '.type-select', this.changeType.bind(this));
-                this.container.on('click', '.add-condition', this.addCondition.bind(this));
-                this.container.on('click', '.add-value', this.addValue.bind(this));
+            },
+            validateElement: function (element) {
+                if (element.classList.contains('__opened')) {
+                    jQuery('#edit_form').valid();
+                    if (!element.select('.fm-block-title-right')[0].hasClassName('warning')) {
+                        if (element.select('.mage-error').length > 0) {
+                            var needToAdd = false;
+                            for (var i = 0; i < element.select('.mage-error').length; i++) {
+                                if (element.select('.mage-error')[i].getStyle('display') !== 'none') {
+                                    needToAdd = true;
+                                }
+                            }
+                            if (needToAdd === true) {
+                                element.select('.fm-block-title-right')[0].addClassName('warning');
+                            }
+                        }
+                    } else {
+                        var needToDelete = true;
+                        for (var i = 0; i < element.select('.mage-error').length; i++) {
+                            if (element.select('.mage-error')[i].getStyle('display') !== 'none') {
+                                needToDelete = false;
+                            }
+                        }
+                        if (needToDelete === true) {
+                            element.select('.fm-block-title-right')[0].removeClassName('warning');
+                        }
+                    }
+                }
             }
         };
         Rows.init();
+        jQuery(function ($) {
+            $('[data-role=rows-container]').sortable({
+                distance: 8,
+                tolerance: 'pointer',
+                cancel: 'input, button, select',
+                axis: 'y',
+                update: function () {
+                    $('[data-role=rows-container] [data-role=order]').each(function (index, element) {
+                        $(element).val(index + 1);
+                    });
+                }
+            });
+        });
     };
 });
