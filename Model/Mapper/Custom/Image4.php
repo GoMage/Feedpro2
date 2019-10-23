1b30e36939ea351004eb635e8d344079ae6f2eab
Rts <rts@gomage.com>
2019-10-23 19:26:27 +0300

    FP2-91 Добавлено можливість виведення зображень з галереї image-image5

---
+++ Model/Mapper/Custom/Image4.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * GoMage.com
+ *
+ * GoMage Feed Pro M2
+ *
+ * @category     Extension
+ * @copyright    Copyright (c) 2010-2018 GoMage.com (https://www.gomage.com)
+ * @author       GoMage.com
+ * @license      https://www.gomage.com/licensing  Single domain license
+ * @terms of use https://www.gomage.com/terms-of-use
+ * @version      Release: 1.2.0
+ * @since        Class available since Release 1.0.0
+ */
+
+namespace GoMage\Feed\Model\Mapper\Custom;
+class Image4 extends Image implements CustomMapperInterface
+{
+    /**
+     * @inheritdoc
+     */
+    public static function getLabel()
+    {
+        return __('Image 4');
+    }
+}
