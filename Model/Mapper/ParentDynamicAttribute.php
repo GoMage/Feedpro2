580ed69ae85d7845d13d79ff61b6084491790536
Mikhail Kulik <mkulik@gomage.com>
2020-06-09 17:18:02 +0700

    FP2-86 Change copyright's version

--- Model/Mapper/ParentDynamicAttribute.php
+++ Model/Mapper/ParentDynamicAttribute.php
@@ -5,11 +5,11 @@
  * GoMage Feed Pro M2
  *
  * @category     Extension
- * @copyright    Copyright (c) 2010-2018 GoMage.com (https://www.gomage.com)
+ * @copyright    Copyright (c) 2010-2020 GoMage.com (https://www.gomage.com)
  * @author       GoMage.com
  * @license      https://www.gomage.com/licensing  Single domain license
  * @terms of use https://www.gomage.com/terms-of-use
- * @version      Release: 1.2.0
+ * @version      Release: 1.3.0
  * @since        Class available since Release 1.0.0
  */
 
