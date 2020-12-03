4bf1de477c316159f037fe9a3ba721a8c172533e
vpritulin <v.pritulin@gmail.com>
2020-12-03 13:46:56 +0200

    fix version

--- Model/Mapper/Custom/Price.php
+++ Model/Mapper/Custom/Price.php
@@ -10,7 +10,7 @@
  * @author       GoMage.com
  * @license      https://www.gomage.com/licensing  Single domain license
  * @terms of use https://www.gomage.com/terms-of-use
- * @version      Release: 1.2.1
+ * @version      Release: 1.3.2
  * @since        Class available since Release 1.3.0
  */
 
